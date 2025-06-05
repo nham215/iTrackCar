d95c1dbbe1b2229d7fbe5e3981ab5349aebfc516
nham215 <142221006+nham215@users.noreply.github.com>
2025-06-05 07:35:09 +0700

    add map

--- lib/screens/tracking_screen.dart
+++ lib/screens/tracking_screen.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class TrackingScreen extends StatelessWidget {
+class TrackingScreen extends StatefulWidget {
   const TrackingScreen({super.key});
 
   @override
+  State<TrackingScreen> createState() => _TrackingScreenState();
+}
+
+class _TrackingScreenState extends State<TrackingScreen> {
+  late GoogleMapController mapController;
+  Set<Marker> markers = {};
+
+  static const CameraPosition initialPosition = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.0,
+  );
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    setState(() {
+      markers.add(
+        const Marker(
+          markerId: MarkerId('vehicle1'),
+          position: LatLng(37.42796133580664, -122.085749655962),
+          infoWindow: InfoWindow(title: 'Vehicle 1'),
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Tracking Screen',
-          style: TextStyle(color: Colors.white),
+    return Scaffold(
+      backgroundColor: const Color(0xFF1E2632),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF2D3545),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: TextField(
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: 'Search VIN',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    prefixIcon: Icon(Icons.search, color: Colors.grey[400]),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+                child: GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: initialPosition,
+                  markers: markers,
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                  zoomControlsEnabled: true,
+                  mapType: MapType.normal,
+                  padding: const EdgeInsets.only(bottom: 16),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
